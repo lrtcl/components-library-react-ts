918cce20796add58318c357a50538640bef40f5d
Laurent Clavier <4008875+lrtcl@users.noreply.github.com>
2022-10-26 18:26:58 +0200

    Add skip links WIP

---
+++ src/components/accessibility/SkipLinks/SkipLinks.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+const SkipLinks: FunctionComponent = () => {
+  return (
+    <div>skip links component</div>
+  )
+};
+
+export default SkipLinks;
